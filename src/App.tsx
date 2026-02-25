c0964e6deefac352bbc0c236a41291be2b2431f8
vkvarun <vkvarun4401@gmail.com>
2026-02-25 15:50:58 +0530

    Initial commit of portfolio project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-5xl font-bold">Tailwind is Working 🚀</h1>
+    </div>
+  )
+}
+
+export default App
